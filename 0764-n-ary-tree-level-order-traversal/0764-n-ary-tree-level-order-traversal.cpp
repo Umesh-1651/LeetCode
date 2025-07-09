8414fcc72e2b1822370651162eedc3cf9ac08a9e
Umesh Hemanth Shivaji <122421297+Umesh-1651@users.noreply.github.com>
2025-07-09 15:33:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ans;
+        queue<Node*> q;
+        if(root == NULL) return {{}};
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> v;
+            for(int i=0;i<size;i++){
+                Node* cur = q.front();
+                q.pop();
+                v.push_back(cur->val);
+                for(auto j:cur->children){
+                    q.push(j);
+                }
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
